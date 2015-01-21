4ba1462c7a27c3c164bb1b762a19ac292ebd664a
adrienLamoureux <vergil1534@gmail.com>
2015-01-21 14:30:53 +0100

    ajout PUT/DEL/POST pour museum

--- client/express/express.js
+++ client/express/express.js
@@ -19,10 +19,61 @@ app.listen(port, function () {
     console.log('Express server listening on port %d in %s mode', port, app.settings.env)
 });
 
+var path = 'http://localhost:8080/rest/museum/'
+
 app.get('/api/museum', function(req,res) {
   console.log('get museums')
-  var newurl = 'http://localhost:8080/rest/museum/museums';
+  var newurl = path + 'museums';
   request.get(newurl, function(error, response, body){
     res.send(body)
   })
 });
+
+app.get('/api/museum/:id', function(req,res) {
+  console.log('get museum ' + req.params.id)
+  var newurl = path+req.params.id;
+  request.get(newurl, function(error, response, body){
+    res.send(body)
+  })
+});
+
+app.post('/api/museum', function(req,res) {
+  console.log('post museum')
+  var newurl = path+'new/museum';
+  request.post({
+    headers: {'content-type' : 'application/json'},
+    url: newurl,
+    json: req.body
+    }, function(error, response, body){
+        console.log(body);
+    }
+  )
+})
+
+app.put('/api/museum/', function(req,res) {
+  console.log('put museum')
+  var newurl = path+'update/museum';
+  request.put({
+    headers: {'content-type' : 'application/json'},
+    method: 'PUT',
+    url: newurl,
+    json: req.body
+    }, function(error, response, body){
+        console.log(body);
+    }
+  )
+})
+
+app.del('/api/museum/', function(req,res) {
+  console.log('delete museum')
+  var newurl = path+'delete/museum';
+  request.put({
+    headers: {'content-type' : 'application/json'},
+    method: 'DELETE',
+    url: newurl,
+    json: req.body
+    }, function(error, response, body){
+        console.log(body);
+    }
+  )
+})
